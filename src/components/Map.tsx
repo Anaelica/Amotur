94928c5ee366df84fd15ad8894439d4a04ab89c7
Anaelica <anaanaelica@gmail.com>
2025-06-24 14:52:00 -0300

    feat: marcadores

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,12 +1,12 @@
+
 'use client';
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
-type PointsType = 'restaurant' | 'bar' | 'hotel';
+type PointsType = 'restaurant' | 'bar' | 'hotel' | 'custom';
 
-// Mapeamento dos ícones por tipo
 const iconMap: Record<PointsType, L.Icon> = {
   restaurant: new L.Icon({
     iconUrl: '/restaurant-coffee-shop-svgrepo-com.png',
@@ -22,6 +22,12 @@ const iconMap: Record<PointsType, L.Icon> = {
   }),
   hotel: new L.Icon({
     iconUrl: 'https://cdn-icons-png.flaticon.com/512/3009/3009489.png',
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+    popupAnchor: [0, -40],
+  }),
+  custom: new L.Icon({
+    iconUrl: 'https://cdn-icons-png.flaticon.com/512/684/684908.png', 
     iconSize: [40, 40],
     iconAnchor: [20, 40],
     popupAnchor: [0, -40],
@@ -42,7 +48,7 @@ const points: Point[] = [
     id: 1,
     name: 'Restaurante Saboroso',
     position: [-3.024930, -39.649836],
-    image: 'https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    image: 'https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?q=80&w=870&auto=format&fit=crop',
     description: 'Um restaurante incrível no centro.',
     type: 'restaurant',
   },
@@ -50,7 +56,7 @@ const points: Point[] = [
     id: 2,
     name: 'Bar do Zé',
     position: [-3.024860, -39.643643],
-    image: 'https://plus.unsplash.com/premium_photo-1661730134261-4381db9b5de3?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    image: 'https://plus.unsplash.com/premium_photo-1661730134261-4381db9b5de3?q=80&w=870&auto=format&fit=crop',
     description: 'O melhor happy hour da cidade.',
     type: 'bar',
   },
@@ -58,9 +64,41 @@ const points: Point[] = [
     id: 3,
     name: 'Hotel Confort',
     position: [-3.034040, -39.654925],
-    image: 'https://images.unsplash.com/photo-1551882547-ff40c63fe5fa?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    image: 'https://images.unsplash.com/photo-1551882547-ff40c63fe5fa?q=80&w=870&auto=format&fit=crop',
     description: 'Hotel confortável e bem localizado.',
     type: 'hotel',
+  },
+  {
+    id: 4,
+    name: 'Casa Zulu',
+    position: [-3.02555, -39.64932],
+    image: '/Casa Zulu.png',
+    description: '',
+    type: 'custom',
+  },
+  {
+    id: 5,
+    name: 'Pousada Maleá',
+    position: [-3.02745, -39.64939],
+    image: '/Pousada Maleá.jpeg',
+    description: '',
+    type: 'custom',
+  },
+  {
+    id: 6,
+    name: 'Icaraí Frios',
+    position: [-3.02826, -39.65019],
+    image: '/Icaraí frios.png',
+    description: '',
+    type: 'custom',
+  },
+  {
+    id: 7,
+    name: 'Casa Arte Vida',
+    position: [-3.02597, -39.64994],
+    image: '/Casa Arte Vida.png',
+    description: '',
+    type: 'custom',
   },
 ];
 
@@ -68,7 +106,7 @@ export default function Map() {
   return (
     <MapContainer
       center={[-3.029350, -39.653422]}
-      zoom={13}
+      zoom={16}
       scrollWheelZoom={true}
       className="w-full h-[600px] rounded-lg shadow-lg"
     >
@@ -91,7 +129,7 @@ export default function Map() {
                 className="w-full h-24 object-cover rounded mb-2"
               />
               <h2 className="font-semibold">{point.name}</h2>
-              <p className="text-sm">{point.description}</p>
+              {point.description && <p className="text-sm">{point.description}</p>}
             </div>
           </Popup>
         </Marker>
